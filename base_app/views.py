16cfb029f90254df91e27255dc2d89ad297c8672
Pavel Olifer <pavel.olifer@gmail.com>
2022-03-07 17:56:07 +0300

    edit templates

--- base_app/views.py
+++ base_app/views.py
@@ -10,8 +10,19 @@ ROOMS = [
 
 
 def home(request):
-    return render(request, 'home.html')
+    context = {
+        'rooms': ROOMS
+    }
+    return render(request, 'base_app/home.html', context=context)
 
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = None
+    for i in ROOMS:
+        if i['id'] == int(pk):
+            room = i
+    context = {
+        'room': room,
+    }
+
+    return render(request, 'base_app/room.html', context)
