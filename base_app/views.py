c0ff23e56eebc5ab261a3be9246927f2c5c3cec2
Pavel Olifer <pavel.olifer@gmail.com>
2022-03-13 21:43:16 +0300

    add crud operations

--- base_app/views.py
+++ base_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
+from .forms import RoomForm
 
 # Create your views here.
 
@@ -7,7 +8,7 @@ from .models import Room
 def home(request):
     rooms = Room.objects.all()
     context = {
-        'rooms': rooms
+        'rooms': rooms,
     }
     return render(request, 'base_app/home.html', context=context)
 
@@ -19,3 +20,44 @@ def room(request, pk):
     }
 
     return render(request, 'base_app/room.html', context)
+
+
+def createRoom(request):
+    form = RoomForm()
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'base_app/room_form.html', context)
+
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'base_app/room_form.html', context)
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+
+    return render(request, 'base_app/delete.html', {'obj': room})
