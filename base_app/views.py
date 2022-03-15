10724c0e354ce7e39a5d16690aa879568eaaf78d
Pavel Olifer <pavel.olifer@gmail.com>
2022-03-15 17:14:35 +0300

    add crud for messages

--- base_app/views.py
+++ base_app/views.py
@@ -8,7 +8,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 from .forms import RoomForm
-from .models import Room, Topic
+from .models import Message, Room, Topic
 
 # Create your views here.
 
@@ -90,8 +90,22 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
+    room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+
     context = {
         'room': room,
+        'room_messages': room_messages,
+        'participants': participants,
     }
 
     return render(request, 'base_app/room.html', context)
@@ -145,3 +159,17 @@ def deleteRoom(request, pk):
         return redirect('home')
 
     return render(request, 'base_app/delete.html', {'obj': room})
+
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('You are not allowed here!!!')
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+
+    return render(request, 'base_app/delete.html', {'obj': message})
