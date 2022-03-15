3e4533a9c6cb08c8231753997835f10b82f9605c
Pavel Olifer <pavel.olifer@gmail.com>
2022-03-15 23:13:06 +0300

    add profile for users

--- base_app/urls.py
+++ base_app/urls.py
@@ -3,10 +3,13 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.home, name='home'),
+
+    path('register/', views.registerPage, name='register'),
     path('login/', views.loginPage, name='login'),
     path('logout/', views.logoutUser, name='logout'),
-    path('register/', views.registerPage, name='register'),
-    path('', views.home, name='home'),
+    path('profile/<str:pk>', views.userProfile, name='user-profile'),
+
     path('room/<str:pk>/', views.room, name='room'),
     path('create-room/', views.createRoom, name='create-room'),
     path('update-room/<str:pk>/', views.updateRoom, name='update-room'),
